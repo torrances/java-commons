69578368e2f1b9e7160a04174249bdb7663a6a2d
Craig Trim <cmtrim@us.ibm.com>
2015-10-21 15:31:46 -0700

    improve adapter method for finding eng grammar upper types

--- src/main/java/com/trimc/blogger/commons/type/adapter/EngGrammarUpperTypeAdapter.java
+++ src/main/java/com/trimc/blogger/commons/type/adapter/EngGrammarUpperTypeAdapter.java
@@ -12,15 +12,79 @@ public final class EngGrammarUpperTypeAdapter {
 
 	public static LogManager logger = new LogManager(EngGrammarUpperTypeAdapter.class);
 
+	public static EngGrammarUpperType findByName(String name) {
+		for (EngGrammarUpperType value : EngGrammarUpperType.values()) {
+			if (value.toString().equalsIgnoreCase(name)) {
+				return value;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * transforms common abbreviations found in etymological sources 
+	 * @param tag
+	 * @return {@link EngGrammarUpperType}
+	 * @throws AdapterValidationException
+	 */
 	public static EngGrammarUpperType findByAbbreviation(String tag) throws AdapterValidationException {
-		tag = tag.toLowerCase();
+		tag = tag.toLowerCase().trim();
 
-		if ("n".equals(tag)) return EngGrammarUpperType.NOUN;
-		if ("n.".equals(tag)) return EngGrammarUpperType.NOUN;
-		if ("n.2".equals(tag)) return EngGrammarUpperType.NOUN;
+		if ("adj.".equals(tag)) return EngGrammarUpperType.ADJECTIVE;
+		if ("adj.1".equals(tag)) return EngGrammarUpperType.ADJECTIVE;
+		if ("adj.2".equals(tag)) return EngGrammarUpperType.ADJECTIVE;
+
+		else if ("adv".equals(tag)) return EngGrammarUpperType.ADVERB;
+		else if ("adv.".equals(tag)) return EngGrammarUpperType.ADVERB;
+		else if ("adv.1".equals(tag)) return EngGrammarUpperType.ADVERB;
+		else if ("adv.2".equals(tag)) return EngGrammarUpperType.ADVERB;
+
+		else if ("conj".equals(tag)) return EngGrammarUpperType.CONJUNCTION;
+		else if ("conj.".equals(tag)) return EngGrammarUpperType.CONJUNCTION;
+
+		else if ("int".equals(tag)) return EngGrammarUpperType.INTERJECTION;
+		else if ("int.".equals(tag)) return EngGrammarUpperType.INTERJECTION;
+		else if ("int.1".equals(tag)) return EngGrammarUpperType.INTERJECTION;
+		else if ("int.2".equals(tag)) return EngGrammarUpperType.INTERJECTION;
+
+		else if ("n".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.1".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.2".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.3".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.4".equals(tag)) return EngGrammarUpperType.NOUN;
+		else if ("n.5".equals(tag)) return EngGrammarUpperType.NOUN;
+
+		else if ("prep".equals(tag)) return EngGrammarUpperType.PREPOSITION;
+		else if ("prep.".equals(tag)) return EngGrammarUpperType.PREPOSITION;
+		else if ("prep.1".equals(tag)) return EngGrammarUpperType.PREPOSITION;
+		else if ("prep.2".equals(tag)) return EngGrammarUpperType.PREPOSITION;
+
+		else if ("pron".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.1".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.2".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.3".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.4".equals(tag)) return EngGrammarUpperType.PRONOUN;
+		else if ("pron.5".equals(tag)) return EngGrammarUpperType.PRONOUN;
+
+		else if ("prefix".equals(tag)) return EngGrammarUpperType.OTHER;
+		else if ("prefix1".equals(tag)) return EngGrammarUpperType.OTHER;
+		else if ("suffix".equals(tag)) return EngGrammarUpperType.OTHER;
+		else if ("suffix1".equals(tag)) return EngGrammarUpperType.OTHER;
+
 		else if ("v".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.1".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.2".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.3".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.4".equals(tag)) return EngGrammarUpperType.VERB;
+		else if ("v.5".equals(tag)) return EngGrammarUpperType.VERB;
 
-		throw new AdapterValidationException("Unrecognized Abbreviation (tag = %s)", tag);
+		else if ("comb. form".equals(tag)) return EngGrammarUpperType.OTHER;
+
+		return null;
 	}
 
 	public static String toString(Collection<EngGrammarUpperType> engGrammarUpperTypes) {
