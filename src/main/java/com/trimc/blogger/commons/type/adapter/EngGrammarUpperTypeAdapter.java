93628c997517c127b151540a1413411e7b61ee25
Craig Trim <cmtrim@us.ibm.com>
2015-10-21 14:57:53 -0700

    improve eng grammar adapter to string methods

--- src/main/java/com/trimc/blogger/commons/type/adapter/EngGrammarUpperTypeAdapter.java
+++ src/main/java/com/trimc/blogger/commons/type/adapter/EngGrammarUpperTypeAdapter.java
@@ -1,5 +1,9 @@
 package com.trimc.blogger.commons.type.adapter;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
 import com.trimc.blogger.commons.LogManager;
 import com.trimc.blogger.commons.exception.AdapterValidationException;
 import com.trimc.blogger.commons.type.EngGrammarUpperType;
@@ -10,10 +14,29 @@ public final class EngGrammarUpperTypeAdapter {
 
 	public static EngGrammarUpperType findByAbbreviation(String tag) throws AdapterValidationException {
 		tag = tag.toLowerCase();
-		
-		if ("n".equals(tag)) return EngGrammarUpperType.NOUN;
-		else if ("v".equals(tag)) return EngGrammarUpperType.VERB;
+
+		if ("n".equals(tag))
+			return EngGrammarUpperType.NOUN;
+		else if ("v".equals(tag))
+			return EngGrammarUpperType.VERB;
 
 		throw new AdapterValidationException("Unrecognized Abbreviation (tag = %s)", tag);
+	}
+
+	public static String toString(Collection<EngGrammarUpperType> engGrammarUpperTypes) {
+		StringBuilder sb = new StringBuilder();
+
+		Iterator<EngGrammarUpperType> iter = engGrammarUpperTypes.iterator();
+		while (iter.hasNext()) {
+			sb.append(iter.next().toString());
+			if (iter.hasNext())
+				sb.append(", ");
+		}
+
+		return sb.toString();
+	}
+
+	public static String toString(EngGrammarUpperType... engGrammarUpperTypes) {
+		return toString(Arrays.asList(engGrammarUpperTypes));
 	}
 }
