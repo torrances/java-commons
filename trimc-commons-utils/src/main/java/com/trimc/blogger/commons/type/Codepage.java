973a28625e3d58e1b12069df00bde1ad0a1125e3
Craig Trim <cmtrim@us.ibm.com>
2016-01-19 15:04:08 -0800

    update codepage w/standard codeset

--- trimc-commons-utils/src/main/java/com/trimc/blogger/commons/type/Codepage.java
+++ trimc-commons-utils/src/main/java/com/trimc/blogger/commons/type/Codepage.java
@@ -1,50 +1,53 @@
 package com.trimc.blogger.commons.type;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import com.trimc.blogger.commons.LogManager;
 
 public enum Codepage {
 
-	ANSII("ansii"),
+	ASCII("ascii", StandardCharsets.US_ASCII),
 
 	/* The Windows-1252 codepage coincides with ISO-8859-1 for all codes except the range 128 to 159 (hex 80 to 9F),
 	 * where the little-used C1 controls are replaced with additional characters including all the missing characters 
 	 * provided by ISO-8859-15. */
-	ISO_8859_1("ISO-8859-1"),
+	ISO_8859_1("ISO-8859-1", StandardCharsets.ISO_8859_1),
 
 	/* Latin with macrons, M�?ori */
-	ISO_8859_13("ISO-8859-13"),
+	ISO_8859_13("ISO-8859-13", null),
 
 	/* Irish (traditional orthography) */
-	ISO_8859_14("ISO-8859-14"),
+	ISO_8859_14("ISO-8859-14", null),
 
 	/* French, Finnish, Estonian */
-	ISO_8859_15("ISO-8859-15"),
+	ISO_8859_15("ISO-8859-15", null),
 
 	/* Romanian, Czech */
-	ISO_8859_2("ISO-8859-2"),
+	ISO_8859_2("ISO-8859-2", null),
 
 	/* Turkish */
-	ISO_8859_3("ISO-8859-3"),
+	ISO_8859_3("ISO-8859-3", null),
 
 	/* Turkish */
-	ISO_8859_9("ISO-8859-9"),
+	ISO_8859_9("ISO-8859-9", null),
 
-	UTF_16BE("utf-16be"),
+	UTF_16BE("utf-16be", StandardCharsets.UTF_16BE),
 
-	UTF_16LE("utf-16le"),
+	UTF_16LE("utf-16le", StandardCharsets.UTF_16LE),
 
-	UTF_32("utf-32"),
+	UTF_32("utf-32", null),
 
-	UTF_32BE("utf-32be"),
+	UTF_32BE("utf-32be", StandardCharsets.UTF_16BE),
 
-	UTF_32LE("utf-32le"),
+	UTF_32LE("utf-32le", StandardCharsets.UTF_16LE),
 
 	/* The Internationally defined Standard ISO/IEC 10646, Universal Character Set (UCS) know as UTF-8 
 	 * ('U' from Universal Character Set + Transformation Format—8-bit) is a variable-width encoding 
 	 * that can represent every character in the Unicode character set. 
 	 * 
 	 * It was designed for backward compatibility with ASCII and to avoid the complications of endianness and byte order marks in UTF-16 and UTF-32. */
-	UTF_8("utf-8"),
+	UTF_8("utf-8", StandardCharsets.UTF_8),
 
 	/* The popular Windows-1252 character set adds all the missing characters provided by ISO/IEC 8859-15, plus a number 
 	 * of typographic symbols, by replacing the rarely used C1 controls in the range 128 to 159 (hex 80 to 9F). 
@@ -54,7 +57,7 @@ public enum Codepage {
 	 * Many web browsers and e-mail clients will interpret ISO-8859-1 control codes as Windows-1252 characters 
 	 * in order to accommodate such mislabeling but it is not standard behaviour and care should be taken to 
 	 * avoid generating these characters in ISO-8859-1 labeled content. */
-	WINDOWS_1252("Windows-1252");
+	WINDOWS_1252("Windows-1252", null);
 
 	public static LogManager logger = new LogManager(Codepage.class);
 
@@ -66,14 +69,29 @@ public enum Codepage {
 		return null;
 	}
 
+	private Charset charset;
+
 	private String name;
 
-	private Codepage(String name) {
+	private Codepage(String name, Charset charset) {
 		setName(name);
+		setCharset(charset);
+	}
+
+	public Charset getCharset() {
+		return charset;
 	}
 
 	private String getName() {
 		return name;
+	}
+
+	public boolean hasCharset() {
+		return null != getCharset();
+	}
+	
+	private void setCharset(Charset charset) {
+		this.charset = charset;
 	}
 
 	private void setName(String name) {
